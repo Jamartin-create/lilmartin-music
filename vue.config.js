68dbbfb37ae11a518d0892894049e8248ac5f78b
LilMartin <martin_du@qq.com>
2022-08-31 18:12:22 +0800

    feat: Electron配置

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,23 @@ module.exports = defineConfig({
   devServer: {
     port: 11001,
   },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        nsis: {
+          allowToChangeInstallationDirectory: true,
+          oneClick: false,
+          installerIcon: "./build/logo.ico", //安装logo
+          installerHeaderIcon: "./build/logo.ico", //安装logo
+        },
+        electronDownload: {
+          mirror: "https://npm.taobao.org/mirrors/electron/", //镜像设置
+        },
+        win: {
+          icon: "./build/logo.ico", //打包windows版本的logo
+        },
+        productName: "LilMartinMusic", //应用的名称
+      },
+    },
+  },
 });
